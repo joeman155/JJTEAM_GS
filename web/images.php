c190c945c7e267207c89c75e889f2570113afdfd
joeman <joeman@850c8bc4-fb21-4573-a561-4e86d6043f32>
2013-08-13 06:07:31 +0000

    * bigger thumbnails * only showing latest 10 images * ordering images by date.

--- web/images.php
+++ web/images.php
@@ -11,9 +11,24 @@ $i=0;
 //try to open the directory
 $dir_handle = @opendir($directory) or die("There is an error with your image directory!");
  
-while ($file = readdir($dir_handle))    //traverse through the files
+$files = array();
+while (false !== ($file = readdir($dir_handle))) {
+       if ($file != "." && $file != "..") {
+          $files[filemtime($directory . "/" . $file)] = $file;
+       }
+   }
+closedir($dir_handle);
+ksort($files);
+
+
+# while ($file = readdir($dir_handle))    //traverse through the files
+$pic_count = 1;
+foreach (array_reverse($files) as $file)
 {
 if($file=='.' || $file == '..') continue;    //skip links to the current and parent directories
+
+if ($pic_count > 10) break;
+++$pic_count;
  
 $file_parts = explode('.',$file);    //split the file name and put each part in an array
 $ext = strtolower(array_pop($file_parts));    //the last element is the extension
@@ -27,7 +42,7 @@ echo "<ul id=\"imagelist\">\n";
 if(in_array($ext,$allowed_types))    //if the extension is an allowable type
 {
 if(($i+1)%4==0) $nomargin='nomargin';    //the last image on the row is assigned the CSS class "nomargin"
-$cmd = 'convert ' . $directory . '/' . $file . ' -thumbnail 80x60 ' . $directory . '/thumbnails/' . $file; 
+$cmd = 'convert ' . $directory . '/' . $file . ' -geometry 160x120 ' . $directory . '/thumbnails/' . $file; 
 
 $thumbnail = $directory . '/thumbnails/' . $file;
 
@@ -37,7 +52,7 @@ if ( ! file_exists($thumbnail))
 }
 
 echo "  <li>\n";
-echo '<a title="'.$title.'" href="'.$directory.'/'.$file.'" target="_blank">'.$title.'<img src="'.$directory.'/thumbnails/'.$file.'"/></a></div>';
+echo '<a title="'.$title.'" href="'.$directory.'/'.$file.'" target="_blank"><img src="'.$directory.'/thumbnails/'.$file.'"/>'.$title.'</a>';
 echo "  </li>\n";
  
 $i++;    //increment the image counter
@@ -45,6 +60,6 @@ $i++;    //increment the image counter
 }
 echo "</ul>\n";
  
-closedir($dir_handle);    //close the directory
+# closedir($dir_handle);    //close the directory
 
 ?>
