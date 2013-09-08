ddfb543ab32cae475ea7b86815c7abbdbaa0e982
joeman <joeman@850c8bc4-fb21-4573-a561-4e86d6043f32>
2013-09-08 22:20:45 +0000

    * Date/time stuff...smaller start script.

--- startap.sh
+++ startap.sh
@@ -10,13 +10,8 @@ echo cape-bone-iio > /sys/devices/bone_capemgr.8/slots
 
 echo Set date and time
 echo ds3232 0x68 > /sys/class/i2c-adapter/i2c-1/new_device
-sleep 1
-modprobe rtc-ds3232
-sleep 1
-hwclock -s -f /dev/rtc1
-sleep 1
-hwclock -w
-sleep 1
+sleep 5
+hwclock -f /dev/rtc1 -s
 
 date
 
