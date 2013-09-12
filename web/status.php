b132b9d116249ab9981ae7b081e2fd31dd3696e2
joeman <joeman@850c8bc4-fb21-4573-a561-4e86d6043f32>
2013-09-12 06:30:59 +0000

    * Fixed LOS...was mixing metres with kms

--- web/status.php
+++ web/status.php
@@ -44,7 +44,7 @@ $gps_creation_date = date("d-m-y H:i:s", strtotime($row['creation_date']));
 if ($latitude != "" && $longitude != "" && $v_local_lat != "" && $v_local_long != "") {
   $v_horizontal_distance = calculateDistance($v_local_lat, $v_local_long, $latitude, $longitude, "K");
   $v_direction = calculateDirection($v_local_lat, $v_local_long, $latitude, $longitude);
-  $v_los_distance = ($v_horizontal_distance ^ 2 + ($height - $v_local_alt)^2)^0.5;
+  $v_los_distance = ($v_horizontal_distance ^ 2 + ($height/1000 - $v_local_alt/1000)^2)^0.5;
   $v_direction = round($v_direction);
   $v_los_distance = round($v_los_distance);
 } else {
