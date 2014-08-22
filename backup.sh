cbad9804211f72ed466c888fad5106f4c8c75121
Joe Turner <joeman@leederville.net>
2014-08-22 20:38:10 +1000

    * Dialogue added to backup.sh

--- backup.sh
+++ backup.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+echo This should be run when BBB is booted from microSD card.
+echo Hit Enter to continue...
+echo
 
+read a
 dd=`date +%d%b%y`
 dd if=/dev/mmcblk1 bs=1M | ssh 192.168.10.122 'dd of=/tmp/habgs-${dd}.img bs=1M'
