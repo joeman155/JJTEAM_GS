ca24492f9a94870bc9675a74c06742ec7577f75e
Joe Turner <joeman@leederville.net>
2014-09-03 12:38:59 +1000

    * Adding code to show path of balloon

---
+++ web/get1DGPS.php
@@ -0,0 +1,50 @@
+<?
+include "config.inc";
+
+# Get all the latest measurements
+ try {
+      $dbh = new PDO("sqlite:" . $db_file);
+     }
+ catch (PDOException $e)
+     {
+      echo $e->getMessage();
+     }
+
+   # GPS
+   $now = date("Y-m-d");
+$now = "2014-09-01";
+   $sql = "select * from gps_t where creation_date >= '" . $now . "'";
+
+   $sth = $dbh->prepare($sql);
+
+   $sth->execute();
+
+   # $row = $sth->fetch();
+
+   $results = array();
+   while ($row = $sth->fetch(PDO::FETCH_ASSOC)) {
+      $latitude = $row['latitude'];
+      $longitude = $row['longitude'];
+      $height = $row['height'];
+      $gps_date = $row['gps_date'];
+      $gps_time = $row['gps_time'];
+      $gps_creation_date = date("d-M-Y H:i:s", $row['creation_date']);
+
+
+      $v['latitude'] = $latitude;
+      $v['longitude'] = $longitude;
+      $v['height'] = $height;
+      $v['gps_date'] = $gps_date;
+      $v['gps_time'] = $gps_time;
+      $v['gps_creation_date'] = $gps_creation_date;
+
+      array_push($results, $v);
+
+   }
+$json = json_encode($results);
+print $json;
+
+$dbh = null;
+
+
+?>
--- web/js/map.js
+++ web/js/map.js
@@ -1,3 +1,6 @@
+			// new OpenLayers.Control.Graticule(true,0.1,0.1,true,true,1,100,"coordinates",true,"dm",-1000,-100)
+                //maxExtent: new OpenLayers.Bounds(-20037508.34, -20037508.34,
+                //                                 20037508.34, 20037508.34),
     function initmap() {
            var options = {
                 projection: new OpenLayers.Projection("EPSG:900913"),
@@ -13,33 +16,68 @@
                         new OpenLayers.Control.Permalink(),
                         new OpenLayers.Control.ScaleLine(),
                         new OpenLayers.Control.MousePosition(),
-                        new OpenLayers.Control.KeyboardDefaults()
+                        new OpenLayers.Control.KeyboardDefaults(),
+			new OpenLayers.Control.Graticule({
+				numPoints: 1,
+				labelled: true,
+				visible: true
+                            })
  
                   ]
             };
         var fromProjection = new OpenLayers.Projection("EPSG:4326");   // Transform from WGS 1984
         var toProjection   = new OpenLayers.Projection("EPSG:900913"); // to Spherical Mercator Projection
-        var position       = new OpenLayers.LonLat(145.3,-17.3838).transform( fromProjection, toProjection);
+        var start_position       = new OpenLayers.LonLat(145.3,-17.3838).transform( fromProjection, toProjection);
         var zoom           = 10;
 
-        var start_lon = 144.8000;
-        var start_lat = -17.3232;
-        var start_position       = new OpenLayers.LonLat(start_lon,start_lat).transform( fromProjection, toProjection);
-        var start1_position       = new OpenLayers.LonLat(start_lon+0.1,start_lat+0.1).transform( fromProjection, toProjection);
 
+	// Balloon tracking icon
         var size = new OpenLayers.Size(21,25);
         var offset = new OpenLayers.Pixel(-(size.w/2), -size.h);
-        var balloon_icon = new OpenLayers.Icon('http://gs.jjteam.net/api/img/balloon.png', size, offset);
-
-
+        var balloon_icon = new OpenLayers.Icon('/api/img/balloon.png', size, offset);
         var markers = new OpenLayers.Layer.Markers( "Markers" );
 
+	// New Map
         map = new OpenLayers.Map("map-canvas",options);
         var newL = new OpenLayers.Layer.OSM("Default", "/osm_tiles/${z}/${x}/${y}.png", {numZoomLevels: 19});
+	var vec = new OpenLayers.Layer.Vector("Overlay");
+
+
+        // Get GPS co-ordinates for the current day.
+	var balloon_gps;
+	var points = [];
+	var style = {strokeColor:"#0500bd", strokeWidth:30};
+        $.ajax({
+                url: 'get1DGPS.php',
+                type: "GET",
+                dataType: "json",
+                async: false,
+                cache: false,
+                success: function(data) {
+			$.each(data, function(index,element) {
+                 		v_lat=element.latitude;
+                	        v_long=element.longitude;
+        			balloon_gps = new OpenLayers.LonLat(v_long,v_lat).transform( fromProjection, toProjection);
+        			markers.addMarker(new OpenLayers.Marker(balloon_gps, balloon_icon.clone()));
+				points.push( new OpenLayers.Geometry.Point(v_long,v_lat).transform( fromProjection, toProjection));
+				});
+                        },
+                failure: function() {
+                        alert('failure when getting latest gps data');
+                        }
+                });
+
+	// Draw balloon path
+	var line = new OpenLayers.Geometry.LineString(points);
+	var fea = new OpenLayers.Feature.Vector(line);
+	vec.addFeatures(fea);
+
+	// Add Layers
         map.addLayer(newL);
         map.addLayer(markers);
-        markers.addMarker(new OpenLayers.Marker(start_position, balloon_icon));
-        markers.addMarker(new OpenLayers.Marker(start1_position, balloon_icon.clone()));
-	map.setCenter(position, zoom);
+        map.addLayer(vec);
+
+	// Center map and zoom in
+	map.setCenter(start_position, zoom);
         map.zoomIn();
  }
